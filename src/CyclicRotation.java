21159b8ee22d34cb87638814935c4a77491a7f56
Jabuya Ojey <elizaphanojey@gmail.com>
2023-08-10 10:09:31 +0300

    CyclicRotation

--- src/CyclicRotation.java
+++ src/CyclicRotation.java
@@ -1,5 +1,8 @@
 public class CyclicRotation {
     public int[] solution(int[] A, int K){
+        if (A.length == 0 || A.length == K){
+            return A;
+        }
         for (int i = 0; i < K; i++){
             int lastInt = A[A.length - 1];
             for (int j = A.length - 2; j >= 0; j--){
